0d48199688ee79ef615802f9ac50b7956fdb0f7b
admixdevops <77119303+admixdevops@users.noreply.github.com>
2021-02-07 03:43:56 +0400

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,2 @@
-sudo apt-get update
-sudo apt-get install curl
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs build-essential npm
-cp .* /ubuntu/test
 cd /ubuntu/test
-npm install
-npm run
+touch test
